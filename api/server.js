84bcaf9e49ce8e0b5cf46fc4902fa6e20e0eb421
Tryon <68423470+tryon-dev@users.noreply.github.com>
2024-06-07 13:55:06 +0200

    First

---
+++ api/server.js
@@ -0,0 +1,33 @@
+const fastify = require('fastify');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = fastify({
+  logger: true,
+});
+
+const fetchData = async (url) => {
+  try {
+    const response = await axios.get(url);
+    return cheerio.load(response.data);
+  } catch (error) {
+    throw new Error('Error fetching the content');
+  }
+};
+
+app.get('/', async (request, reply) => {
+  try {
+    const $ = await fetchData('https://neat-footprint-573871.framer.app/');
+    reply.type('text/html').send($.html());
+  } catch (error) {
+    reply.code(500).send(error.message);
+  }
+});
+
+app.listen((err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  app.log.info(`Server is running on ${address}`);
+});
